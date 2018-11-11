75faf8fbde94941ea84f37d44fdc78a34b7b1e7c
anishbnair <nbanish@gmail.com>
2018-11-11 18:55:19 -0500

    added a constructor

--- Selenium_POM/src/com/gmail/pom/LandingPage.java
+++ Selenium_POM/src/com/gmail/pom/LandingPage.java
@@ -1,5 +1,18 @@
 package com.gmail.pom;
 
+import org.openqa.selenium.WebDriver;
+
 public class LandingPage {
+	
+	WebDriver driver;
+	
+	// Constructor
+	public LandingPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+	
+	
 
 }
