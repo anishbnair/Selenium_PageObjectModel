606eea3a85e3087c8ad7a6f0148f2683e30216d1
anishbnair <nbanish@gmail.com>
2018-11-11 19:41:37 -0500

    created a constructor called LoginPage

--- Selenium_POM/src/com/gmail/pom/LoginPage.java
+++ Selenium_POM/src/com/gmail/pom/LoginPage.java
@@ -1,5 +1,19 @@
 package com.gmail.pom;
 
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+	
+	WebDriver driver;
+	
+	// Constructor
+	public LoginPage(WebDriver driver) {		
+		this.driver = driver;		
+	}
+	
+	// Locator for email
+	
+	
+	
 
 }
