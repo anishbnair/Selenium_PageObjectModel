f8a6689a68d5bed32e673f54c151d564832c225e
anishbnair <nbanish@gmail.com>
2018-11-11 20:21:33 -0500

    created a main method

--- Selenium_POM/src/com/gmail/pom/LoginTest.java
+++ Selenium_POM/src/com/gmail/pom/LoginTest.java
@@ -1,5 +1,17 @@
 package com.gmail.pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://gmail.com");
+	}
 
 }
